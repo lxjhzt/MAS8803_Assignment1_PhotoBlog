f0e6358a292898fd361a32c26897a1e623a859bc
lxjhzt <lxjhzt@users.noreply.github.com>
2018-09-03 07:09:55 -0400

    test

--- app/src/main/java/com/example/mas01/photoblog/MainActivity.java
+++ app/src/main/java/com/example/mas01/photoblog/MainActivity.java
@@ -3,28 +3,16 @@ package com.example.mas01.photoblog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar mainToolbar;
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-
-        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        setSupportActionBar(mainToolbar);
-        getSupportActionBar().setTitle("Photo Blog");
-
     }
 
     @Override
@@ -33,37 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser == null) {
-            sendToLogout();
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
         }
     }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_logout_btn:
-                logOut();
-                return true;
-                default:
-                    return false;
-        }
-    }
-
-    private void logOut() {
-        mAuth.signOut();
-        sendToLogout();
-    }
-
-    private void sendToLogout() {
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(loginIntent);
-        finish();
-    }
-
 }
